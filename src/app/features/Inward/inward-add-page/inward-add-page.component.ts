cddaac62818337572704f279e41433588e7973c9
Abhay4155 <136358047+Abhay4155@users.noreply.github.com>
2025-06-28 23:45:10 +0530

    Changes

--- src/app/features/Inward/inward-add-page/inward-add-page.component.ts
+++ src/app/features/Inward/inward-add-page/inward-add-page.component.ts
@@ -11,33 +11,69 @@ export class InwardAddPageComponent implements OnInit {
 
   onSubmit(form: any) {
     console.log(form, 'onSubmit');
+
+    if (form.lastname === 'Select team size') {
+      alert('Please select a valid team size');
+      return;
+    }
+    
   }
 
   ngOnInit(): void {
-    this.form_service.buildForm({
-      name: {
-        type: "text",
-        value: "",
-        label: "your label name",
-        rules: {
-          required: true,
+    this.form_service.buildForm(
+      {
+        name: {
+          type: "text",
+          value: "",
+          label: "Your Label Name",
+          rules: { required: true },
+          placeholderText: "",
+          maxLength: 6,
+          ngClass: "col-md-6 form-field"
         },
-        placeholderText: "",
-        maxLength: 6
+        lastname: {
+          type: "SelectOption",
+          value: "Select team size",
+          label: "Your Last Name",
+          rules: { required: true },
+          items: ["Select team size", "add", "sdsdsds"],
+          ngClass: "col-md-6 field-lastname form-field"
+        },
+        lastnames: {
+          type: "SelectOption",
+          value: "Select team size",
+          label: "Your Last Name",
+          rules: { required: true },
+          items: ["Select team size", "add", "sdsdsds"],
+          ngClass: "col-md-6 field-lastname form-field"
+        },
+        lastnamee: {
+          type: "SelectOption",
+          value: "Select team size",
+          label: "Your Last Name",
+          rules: { required: true },
+          items: ["Select team size", "add", "sdsdsds"],
+          ngClass: "col-md-6 field-lastname form-field"
+        },
+        lastnamea: {
+          type: "SelectOption",
+          value: "Select team size",
+          label: "Your Last Name",
+          rules: { required: true },
+          items: ["Select team size", "add", "sdsdsds"],
+          ngClass: "col-md-6 field-lastname form-field"
+        },
+        lastnamess: {
+          type: "SelectOption",
+          value: "Select team size",
+          label: "Your Last Name",
+          rules: { required: true },
+          items: ["Select team size", "add", "sdsdsds"],
+          ngClass: "col-md-6 field-lastname form-field"
+        }
       },
-      lastname: {
-        type: "SelectOption",
-        value: "",
-        label: "your last name",
-        rules: {
-          required: true,
-        },
-        placeholderText: "",
-        maxLength: 6,
-        items: ['add', 'sdsdsds'],
-        ngClass: "dropdown"
-      }
-    }, "newform").then((res: any) => {
-    });
+      "newform"
+    ).then((res: any) => {});
   }
+  
 }
